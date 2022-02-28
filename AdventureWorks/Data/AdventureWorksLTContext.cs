729d0b8f238bd99097466475b0b822a2b3d28f7d
nopetski <noora.kiilakoski@gmail.com>
2022-02-28 14:16:05 +0200

    Existing code

---
+++ AdventureWorks/Data/AdventureWorksLTContext.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace AdventureWorks.Data
+{
+    public partial class AdventureWorksLTContext : DbContext
+    {
+        public AdventureWorksLTContext()
+        {
+        }
+
+        public AdventureWorksLTContext(DbContextOptions<AdventureWorksLTContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Address> Addresses { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductCategory> ProductCategories { get; set; }
+        public virtual DbSet<ProductDescription> ProductDescriptions { get; set; }
+        public virtual DbSet<ProductModel> ProductModels { get; set; }
+        public virtual DbSet<ProductModelProductDescription> ProductModelProductDescriptions { get; set; }
+        public virtual DbSet<SalesOrderDetail> SalesOrderDetails { get; set; }
+        public virtual DbSet<SalesOrderHeader> SalesOrderHeaders { get; set; }
+        public virtual DbSet<VGetAllCategory> VGetAllCategories { get; set; }
+        public virtual DbSet<VProductAndDescription> VProductAndDescriptions { get; set; }
+        public virtual DbSet<VProductModelCatalogDescription> VProductModelCatalogDescriptions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:sav-mik-devsql.database.windows.net,1433;Initial Catalog=AdventureWorksLT;Persist Security Info=False;User ID=ets730021s;Password=qMA9m7b4ZGfEgWsM;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.PasswordHash).IsUnicode(false);
+
+                entity.Property(e => e.PasswordSalt).IsUnicode(false);
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+            });
+
+            modelBuilder.Entity<CustomerAddress>(entity =>
+            {
+                entity.HasKey(e => new { e.CustomerId, e.AddressId })
+                    .HasName("PK_CustomerAddress_CustomerID_AddressID");
+
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+
+                entity.HasOne(d => d.Address)
+                    .WithMany(p => p.CustomerAddresses)
+                    .HasForeignKey(d => d.AddressId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.CustomerAddresses)
+                    .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+            });
+
+            modelBuilder.Entity<ProductCategory>(entity =>
+            {
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+
+                entity.HasOne(d => d.ParentProductCategory)
+                    .WithMany(p => p.InverseParentProductCategory)
+                    .HasForeignKey(d => d.ParentProductCategoryId)
+                    .HasConstraintName("FK_ProductCategory_ProductCategory_ParentProductCategoryID_ProductCategoryID");
+            });
+
+            modelBuilder.Entity<ProductDescription>(entity =>
+            {
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+            });
+
+            modelBuilder.Entity<ProductModel>(entity =>
+            {
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+            });
+
+            modelBuilder.Entity<ProductModelProductDescription>(entity =>
+            {
+                entity.HasKey(e => new { e.ProductModelId, e.ProductDescriptionId, e.Culture })
+                    .HasName("PK_ProductModelProductDescription_ProductModelID_ProductDescriptionID_Culture");
+
+                entity.Property(e => e.Culture).IsFixedLength(true);
+
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+
+                entity.HasOne(d => d.ProductDescription)
+                    .WithMany(p => p.ProductModelProductDescriptions)
+                    .HasForeignKey(d => d.ProductDescriptionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.ProductModel)
+                    .WithMany(p => p.ProductModelProductDescriptions)
+                    .HasForeignKey(d => d.ProductModelId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            modelBuilder.Entity<SalesOrderDetail>(entity =>
+            {
+                entity.HasKey(e => new { e.SalesOrderId, e.SalesOrderDetailId })
+                    .HasName("PK_SalesOrderDetail_SalesOrderID_SalesOrderDetailID");
+
+                entity.Property(e => e.SalesOrderDetailId).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.LineTotal).HasComputedColumnSql("(isnull(([UnitPrice]*((1.0)-[UnitPriceDiscount]))*[OrderQty],(0.0)))", false);
+
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.SalesOrderDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            modelBuilder.Entity<SalesOrderHeader>(entity =>
+            {
+                entity.HasKey(e => e.SalesOrderId)
+                    .HasName("PK_SalesOrderHeader_SalesOrderID");
+
+                entity.Property(e => e.SalesOrderId).HasDefaultValueSql("(NEXT VALUE FOR [SalesLT].[SalesOrderNumber])");
+
+                entity.Property(e => e.CreditCardApprovalCode).IsUnicode(false);
+
+                entity.Property(e => e.Freight).HasDefaultValueSql("((0.00))");
+
+                entity.Property(e => e.ModifiedDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.OnlineOrderFlag).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.OrderDate).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Rowguid).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.SalesOrderNumber).HasComputedColumnSql("(isnull(N'SO'+CONVERT([nvarchar](23),[SalesOrderID],(0)),N'*** ERROR ***'))", false);
+
+                entity.Property(e => e.Status).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.SubTotal).HasDefaultValueSql("((0.00))");
+
+                entity.Property(e => e.TaxAmt).HasDefaultValueSql("((0.00))");
+
+                entity.Property(e => e.TotalDue).HasComputedColumnSql("(isnull(([SubTotal]+[TaxAmt])+[Freight],(0)))", false);
+
+                entity.HasOne(d => d.BillToAddress)
+                    .WithMany(p => p.SalesOrderHeaderBillToAddresses)
+                    .HasForeignKey(d => d.BillToAddressId)
+                    .HasConstraintName("FK_SalesOrderHeader_Address_BillTo_AddressID");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.SalesOrderHeaders)
+                    .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.ShipToAddress)
+                    .WithMany(p => p.SalesOrderHeaderShipToAddresses)
+                    .HasForeignKey(d => d.ShipToAddressId)
+                    .HasConstraintName("FK_SalesOrderHeader_Address_ShipTo_AddressID");
+            });
+
+            modelBuilder.Entity<VGetAllCategory>(entity =>
+            {
+                entity.ToView("vGetAllCategories", "SalesLT");
+            });
+
+            modelBuilder.Entity<VProductAndDescription>(entity =>
+            {
+                entity.ToView("vProductAndDescription", "SalesLT");
+
+                entity.Property(e => e.Culture).IsFixedLength(true);
+            });
+
+            modelBuilder.Entity<VProductModelCatalogDescription>(entity =>
+            {
+                entity.ToView("vProductModelCatalogDescription", "SalesLT");
+
+                entity.Property(e => e.ProductModelId).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.HasSequence<int>("SalesOrderNumber", "SalesLT");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ AdventureWorks/Program.cs
@@ -0,0 +1,112 @@
+﻿using AdventureWorks.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+
+namespace AdventureWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("----- YHDISTÄÄ -----");
+
+            Console.WriteLine(ProductCategoryList(new AdventureWorksLTContext())); // haetaan tiedot
+
+            Console.WriteLine(ProductModelList(new AdventureWorksLTContext(), 3)); // haetaan tiedot
+
+            // SqlQuery(new AdventureWorksLTContext(), 1);
+
+            DbDataReader reader = SqlQuery(new AdventureWorksLTContext(), 29485);
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetString(0)} {reader.GetString(1)}, {reader.GetString(2)}, {reader.GetString(3)}");
+            }
+
+            Console.WriteLine("----- VALAMIS -----");
+
+        }
+
+        static string ProductCategoryList(AdventureWorksLTContext context)
+        {
+            string palautettavaMerkkijono = "";
+
+            // haetaan listaan kaikki parent product categories
+            var allParentProductCategories = context.ProductCategories
+                .Where(p => p.ParentProductCategory == null)
+                .ToList();
+
+            // jokaiselle parent product categorylle haetaan subcategoryt
+            foreach (ProductCategory p in allParentProductCategories)
+            {
+                palautettavaMerkkijono = palautettavaMerkkijono + "--" + p.ProductCategoryId + ": " + p.Name + "--\n"; // lisätään parent
+
+                var allSubcategoriesOfParent = context.ProductCategories
+                    .Where(c => c.ParentProductCategoryId == p.ProductCategoryId)
+                    .ToList(); // haetaan subcategories
+
+                foreach (ProductCategory s in allSubcategoriesOfParent)
+                {
+                    palautettavaMerkkijono = palautettavaMerkkijono + "\t" + s.ProductCategoryId + ": " + s.Name + "\n"; // lisätään jokainen subcategory
+                }
+            }
+
+            return palautettavaMerkkijono;
+        }
+
+
+        static string ProductModelList(AdventureWorksLTContext context, int maara)
+        {
+            string palautettavaMerkkijono = "";
+
+            var allProductModels = context.ProductModels // hakee kaikki modelit
+                .OrderBy(model => model.Name) // aakkosjärjestyksessä
+                .ToList();
+
+            for (int i = 0; i < maara; i++) // hakee halutun määrän descriptioneita
+            {
+                palautettavaMerkkijono = palautettavaMerkkijono + allProductModels[i].Name + ": "; // lisää modelin nimen
+
+                var modelDescriptionId = context.ProductModelProductDescriptions
+                    .Where(md => md.ProductModelId == allProductModels[i].ProductModelId) // mikä model
+                    .Where(md => md.Culture == "en") // englanniksi vain
+                    .Select(md => md.ProductDescriptionId) // mikä id toisessa taulussa
+                    .ToList();
+
+                var description = context.ProductDescriptions // hakee kuvauksen
+                    .Where(d => d.ProductDescriptionId == modelDescriptionId[0])
+                    .Select(d => d.Description)
+                    .ToList();
+
+                palautettavaMerkkijono = palautettavaMerkkijono + description[0] + "\n"; // lisää kuvauksen
+            }
+
+            return palautettavaMerkkijono;
+        }
+
+        static DbDataReader SqlQuery(AdventureWorksLTContext context, int id)
+        {
+            string queryString = "SELECT c.FirstName, c.LastName, a.AddressLine1, a.City " +
+                "FROM SalesLT.Customer c " +
+                "JOIN SalesLT.CustomerAddress ca ON c.CustomerId = ca.CustomerID " +
+                "JOIN SalesLT.Address a On ca.AddressID = a.AddressID " +
+                "WHERE c.CustomerID=" + id;
+
+            var connection = context.Database.GetDbConnection();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = queryString;
+                connection.Open();
+                return command.ExecuteReader();
+                
+            }     
+        }
+    }
+}
